b779ed73740cf7f042c8ba8e6c27c4c2ccd1471f
Sergey Kuzmich <7501301@gmail.com>
2013-05-05 21:18:06 +0300

    New XML Structure

---
+++ zip.php
@@ -0,0 +1,30 @@
+<?php
+
+define('ROOT', dirname(__FILE__).'/');
+
+$path = $_GET['folder'];
+
+$zip = new ZipArchive();
+$filename = "./cache/".uniqid().".zip";
+
+if ($zip->open($filename, ZIPARCHIVE::CREATE)!==TRUE) {
+    exit("Cannot open <$filename>\n");
+}
+
+$files = glob(ROOT.$path."/*");
+foreach($files as $key => $value)
+{
+	$fileinfo = pathinfo($value);
+	$zip->addFile($value, $key.".".$fileinfo['extension']);
+}
+$zip->close();
+
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="chapter.zip"');
+header('Content-Transfer-Encoding: binary');
+header('Content-Length: '.filesize($filename));
+readfile($filename);
+unlink($filename);
+
+?>
